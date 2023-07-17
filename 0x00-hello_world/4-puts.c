8753c93ef968d23be5a13027fa64868b275003d0
imane <imanehds76@gmail.com>
2023-07-17 16:13:53 -0700

    taks4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,7 @@
-#!/bin/bash
-int main() { return puts("Programming is like building a multilingual puzzle"); }
+#include <stdio.h>
+
+int main() {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
 
