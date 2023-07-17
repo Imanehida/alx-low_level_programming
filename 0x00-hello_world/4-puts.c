7e29ce86df3bc87a2f672e77470234986f4d776e
imane <imanehds76@gmail.com>
2023-07-17 16:29:49 -0700

    taks4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
 
