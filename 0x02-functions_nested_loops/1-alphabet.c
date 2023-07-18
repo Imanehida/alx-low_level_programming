030e21bb627d69dbe13a211f8c2ce82eb9307838
imane <imanehds76@gmail.com>
2023-07-18 16:13:30 -0700

    01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+
+int main() {
+    print_alphabet();
+    return 0;
+}
+
+void print_alphabet(void) {
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++) {
+        _putchar(c);
+    }
+
+    _putchar('\n');
+}
