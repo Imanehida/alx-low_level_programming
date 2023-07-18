c01bd45de06006741bf22fa9210f6e3da2ecd230
imane <imanehds76@gmail.com>
2023-07-17 17:04:05 -0700

    taks8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific message to the standard error
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = sizeof(message) - 1;
+
+write(2, message, len);
+return (1);
+}
