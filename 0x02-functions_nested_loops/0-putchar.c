3134eec1740431598c7d827e12e6eaf259cdc97f
imane <imanehds76@gmail.com>
2023-07-18 15:42:48 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("_putchar\n");
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
 }
