d29c3a7ae2a5baab2c6be47ae8146d913d1d4b3b
imane <imanehds76@gmail.com>
2023-07-19 06:42:27 -0700

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,17 +5,14 @@
  */
 int main(void)
 {
-    int sum = 0;
-    int i;
-
-    for (i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
-
-    printf("%d\n", sum);
-    return 0;
+int sum = 0;
+int i;
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{sum += i;
+}
+}
+printf("%d\n", sum);
+return 0;
 }
