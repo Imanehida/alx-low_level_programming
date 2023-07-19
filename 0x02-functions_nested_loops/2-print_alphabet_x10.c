f34b84404ab7b1fa7dfef7ac1096ba2844905123
imane <imanehds76@gmail.com>
2023-07-18 17:23:17 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,18 @@
 
 void print_alphabet_x10(void) {
     char letter = 'a';
+    int i, j;
 
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 26; j++) {
-           putchar(letter);
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 26; j++) {
+            putchar(letter);
             letter++;
         }
         putchar('\n');
-        letter = 'a'; // Reset the letter to 'a' for the next line
+        letter = 'a';
     }
-}int main(void) {
+}
+int main(void) {
     print_alphabet_x10();
     return 0;
 }
