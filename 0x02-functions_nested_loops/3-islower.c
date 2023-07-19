8e1ca67ec76d64df7777d26fd48a547bd56b5cec
imane <imanehds76@gmail.com>
2023-07-19 04:51:15 -0700

    03

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,16 +13,3 @@ int _islower(int c) {
     else
         return 0;
 }
-
-int main(void) {
-    int r;
-
-    r = _islower('H');
-    putchar(r + '0');
-    r = _islower('o');
-    putchar(r + '0');
-    r = _islower(108);
-    putchar(r + '0');
-    putchar('\n');
-    return 0;
-}
