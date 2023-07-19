e6ebfd1eb1b1266ab2584beca81eced6760d4ed3
imane <imanehds76@gmail.com>
2023-07-19 16:39:33 -0700

    Tasks1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,15 @@
-#include <stdio.h>
-/* Function to test if a number is positive or negative */
-const char *positive_or_negative(int number)
-{
-if (number > 0)
-{
-return "Positive";
-}
-else if (number < 0)
-{
-return "Negative";
-}
-else
-{
-return "Zero";
-}
-}
+#include "main.h"
+/**
+ * main - Tests the function that prints if an integer is positive or negative
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-/* Test cases */
-int num1 = 5;
-int num2 = -2;
-int num3 = 0;
-printf("%d is %s\n", num1, positive_or_negative(num1)); /* Output: "5 is Positive" */
-printf("%d is %s\n", num2, positive_or_negative(num2)); /* Output: "-2 is Negative" */
-printf("%d is %s\n", num3, positive_or_negative(num3)); /* Output: "0 is Zero" */
-return 0;
+    int i;
+
+    i = 98;
+    positive_or_negative(i);
+
+    return (0);
 }
