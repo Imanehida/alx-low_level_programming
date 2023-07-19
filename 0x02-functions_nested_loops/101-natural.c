2c37e77eb4057ced197b5e0fba7e3d302e09db9a
imane <imanehds76@gmail.com>
2023-07-19 07:13:41 -0700

    14

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,14 +7,14 @@
 int main(void)
 {
     int count;
-    unsigned long int n1 = 1, n2 = 2, next;
+    unsigned long long int n1 = 1, n2 = 2, next;
 
-    printf("%lu, %lu, ", n1, n2);
+    printf("%llu, %llu, ", n1, n2);
 
     for (count = 3; count <= 50; count++)
     {
         next = n1 + n2;
-        printf("%lu", next);
+        printf("%llu", next);
 
         if (count < 50)
             printf(", ");
