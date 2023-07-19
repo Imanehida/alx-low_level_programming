c1caf9455f16c642f00339902979bf5437ceab91
imane <imanehds76@gmail.com>
2023-07-19 08:45:35 -0700

    advanced1

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
-{int fibonacci[50];
-int i;
-fibonacci[0] = 1;
-fibonacci[1] = 2;
-for (i = 2; i < 50; i++)
 {
-fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-}
-for (i = 0; i < 49; i++)
-{
-printf("%d, ", fibonacci[i]);
-}
-printf("%d\n", fibonacci[49]);
-return (0);
+    long long int fibonacci[50];
+    int i;
+
+    fibonacci[0] = 1;
+    fibonacci[1] = 2;
+
+    for (i = 2; i < 50; i++)
+    {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+
+    for (i = 0; i < 49; i++)
+    {
+        printf("%lld, ", fibonacci[i]);
+    }
+    
+    printf("%lld\n", fibonacci[49]);
+
+    return (0);
 }
