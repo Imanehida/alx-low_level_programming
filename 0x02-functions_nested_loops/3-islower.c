3499f604851e72f7396bc1314fb2c52e55905b42
imane <imanehds76@gmail.com>
2023-07-18 17:53:14 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,21 @@
+#include <stdio.h>
+
 int _islower(int c) {
     if (c >= 'a' && c <= 'z') {
-        return 1; // Character is lowercase
+        return 1; /* Caractère en minuscule */
     } else {
-        return 0; // Character is not lowercase
+        return 0; /* Caractère non en minuscule */
     }
+}
+
+int main(void) {
+    char character = 'A';
+
+    if (_islower(character)) {
+        printf("Le caractère %c est en minuscule.\n", character);
+    } else {
+        printf("Le caractère %c n'est pas en minuscule.\n", character);
+    }
+
+    return 0;
 }
