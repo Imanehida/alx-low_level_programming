159bead7304bca7be291043a02bc04c7daea5340
imane <imanehds76@gmail.com>
2023-07-19 04:29:10 -0700

    03

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,4 @@
-/* _islower.c */
-#include "main.h"
+#include <stdio.h>
 
 /**
  * _islower - Checks if a character is lowercase.
@@ -7,39 +6,23 @@
  *
  * Return: 1 if c is lowercase, 0 otherwise.
  */
-int _islower(int c)
-{
+int _islower(int c) {
     /* ASCII value of lowercase alphabets ranges from 97 to 122 */
     if (c >= 'a' && c <= 'z')
         return 1;
     else
         return 0;
 }
-/* _putchar.c */
-#include <unistd.h>
 
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-/* 3-main.c */
-#include "main.h"
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
+int main(void) {
     int r;
 
     r = _islower('H');
-    _putchar(r + '0');
+    putchar(r + '0');
     r = _islower('o');
-    _putchar(r + '0');
+    putchar(r + '0');
     r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+    putchar(r + '0');
+    putchar('\n');
+    return 0;
 }
