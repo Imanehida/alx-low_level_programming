4cd189ea82e2de7ed13e474cc5e26a2209c097fc
imane <imanehds76@gmail.com>
2023-07-19 08:31:15 -0700

    taks15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,22 +3,23 @@
 
 int main(void)
 {
-    long int fib[3] = {1, 2, 0}; 
-    long int sum = 2; 
+    long int fib[3] = {1, 2, 0};
+    long int sum = 2;
 
     while (fib[2] < 4000000)
     {
-        fib[2] = fib[0] + fib[1]; 
+        fib[2] = fib[0] + fib[1];
 
         if (fib[2] % 2 == 0)
         {
-            sum += fib[2]; 
+            sum += fib[2];
+        }
 
-        fib[0] = fib[1]; 
-        fib[1] = fib[2]; 
+        fib[0] = fib[1];
+        fib[1] = fib[2];
     }
 
-    printf("%lu\n", sum); 
+    printf("%lu\n", sum);
 
     return (0);
 }
