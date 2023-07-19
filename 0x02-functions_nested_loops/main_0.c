5a61c937ebe1d61b956d48c18475bec6ccd22a62
Imanehida <136011022+Imanehida@users.noreply.github.com>
2023-07-19 13:07:49 +0200

    Update main_0.c

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+void test_lower(int n)
+{
+	int r;
+
+	r = lower(n);
+	putchar(r + '0');
+	putchar('\n');
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	test_lower('H');
+	return (0);
+}
