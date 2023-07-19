084e9b6c1e2750fcef60cbd486c3ef4ed9985e22
imane <imanehds76@gmail.com>
2023-07-19 07:11:27 -0700

    14

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0
  */
 int main(void)
 {
-int count, n1 = 1, n2 = 2, next;
-printf("%d, %d, ", n1, n2);
-for (count = 3; count <= 50; count++)
-{
-next = n1 + n2;
-printf("%d", next);
-if (count < 50)
-printf(", ");
-n1 = n2;
-n2 = next;
-}
-printf("\n");
-return 0;
+    int count;
+    unsigned long int n1 = 1, n2 = 2, next;
+
+    printf("%lu, %lu, ", n1, n2);
+
+    for (count = 3; count <= 50; count++)
+    {
+        next = n1 + n2;
+        printf("%lu", next);
+
+        if (count < 50)
+            printf(", ");
+
+        n1 = n2;
+        n2 = next;
+    }
+
+    printf("\n");
+    return (0);
 }
