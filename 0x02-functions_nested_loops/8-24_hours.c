cbc1a723beb4b9b2b009a8aa4935c35c8432b528
imane <imanehds76@gmail.com>
2023-07-19 05:42:24 -0700

    08

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,7 @@
  * jack_bauer - Prints every minute of the day of Jack Bauer.
  */
 void jack_bauer(void)
-{
-	int hour, minute;
+{	int hour, minute;
 	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
