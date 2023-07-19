6d277ea2e733a699b267b5a9873bc58490285044
imane <imanehds76@gmail.com>
2023-07-19 16:21:09 -0700

    Tasks1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-/** Function to test if a number is positive or negative */
-const char* positive_or_negative(int number) {
-    if (number > 0) {
-        return "Positive";
-    } else if (number < 0) {
-        return "Negative";
-    } else {
-        return "Zero";
-    }
+/* Function to test if a number is positive or negative */
+const char *positive_or_negative(int number)
+{
+if (number > 0)
+{
+return "Positive";
 }
-int main() {
-    
-    int num1 = 5;
-    int num2 = -2;
-    int num3 = 0;
-
-    printf("%d is %s\n", num1, positive_or_negative(num1)); 
-    printf("%d is %s\n", num2, positive_or_negative(num2)); 
-    printf("%d is %s\n", num3, positive_or_negative(num3)); 
-
-    return 0;
+else if (number < 0)
+{
+return "Negative";
+}
+else
+{
+return "Zero";
+}
+}
+int main(void)
+{
+/* Test cases */
+int num1 = 5;
+int num2 = -2;
+int num3 = 0;
+printf("%d is %s\n", num1, positive_or_negative(num1)); /* Output: "5 is Positive" */
+printf("%d is %s\n", num2, positive_or_negative(num2)); /* Output: "-2 is Negative" */
+printf("%d is %s\n", num3, positive_or_negative(num3)); /* Output: "0 is Zero" */
+return 0;
 }
