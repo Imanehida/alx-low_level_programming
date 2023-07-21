73b621ef33357fddd0dc5d9f098ef8525dbb99ef
imane <imanehds76@gmail.com>
2023-07-20 17:24:47 -0700

    task4

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,33 +10,21 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
+int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 {
-if (month > 2)
+days_in_month[2] = 29; // February has 29 days in a leap year
+}
+if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
 {
-day++; /* Adjust for the extra day in a leap year */
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+return;
 }
-if (day > 60)
+int day_of_year = day;
+for (int i = 1; i < month; i++)
 {
-printf("Day of the year: %d\n", day - 1); /* Adjust for the extra day in a leap year */
-printf("Remaining days: %d\n", 366 - day);
+day_of_year += days_in_month[i];
 }
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
-}
-}
-else
-{
-if (day > 59) /* February 29th is invalid in non-leap years */
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
-}
-}
+printf("Day of the year: %d\n", day_of_year);
+printf("Remaining days: %d\n", 365 + ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) - day_of_year);
 }
